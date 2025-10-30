825bc2e64adb38ed26fe7e3bd24a2317709bd83b
AndreyProgger <a91513957@gmail.com>
2025-10-30 20:36:58 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -9,9 +9,10 @@ document.addEventListener('DOMContentLoaded', () => {
       e.preventDefault()
       const page = link.getAttribute('data-page')
       
-      // Для GitHub Pages используем относительные пути
+      // Для GitHub Pages используем полные пути
       if (window.location.hostname.includes('github.io')) {
-        window.location.href = `./${page}/${page}.html`
+        const repoName = 'my-vite-app' 
+        window.location.href = `/${repoName}/${page}/${page}.html`
       } else {
         // Для локальной разработки
         window.location.href = `/src/pages/${page}/${page}.html`
